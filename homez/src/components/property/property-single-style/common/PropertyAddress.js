775790f0cfc2e51515cf4ba1ce9150cc2216bb64
omrifahn <omrifahn@gmail.com>
2023-09-27 01:25:59 +0300

    property address get params

--- homez/src/components/property/property-single-style/common/PropertyAddress.js
+++ homez/src/components/property/property-single-style/common/PropertyAddress.js
@@ -1,25 +1,13 @@
 import React from "react";
 
-const PropertyAddress = () => {
-  const addresses = [
+const PropertyAddress = ({
+  addresses = [
     {
-      address: "10425 Tabor St",
-      city: "Los Angeles",
-      state: "California",
-      zipCode: "90034",
-      area: "Brookside",
-      country: "United States",
+      address: "Stralauer 14, Berlin",
+      district: "Friedrichshain",
     },
-    {
-      address: "10 Downing Street",
-      city: "London",
-      state: "Greater London",
-      zipCode: "SW1A 2AA",
-      area: "Westminster",
-      country: "United Kingdom",
-    },
-  ];
-
+  ],
+}) => {
   return (
     <>
       {addresses.map((address, index) => (
@@ -30,28 +18,27 @@ const PropertyAddress = () => {
           <div className="d-flex justify-content-between">
             <div className="pd-list">
               <p className="fw600 mb10 ff-heading dark-color">Address</p>
-              <p className="fw600 mb10 ff-heading dark-color">City</p>
-              <p className="fw600 mb-0 ff-heading dark-color">State/county</p>
+              <p className="fw600 mb10 ff-heading dark-color">District</p>
             </div>
             <div className="pd-list">
               <p className="text mb10">{address.address}</p>
-              <p className="text mb10">{address.city}</p>
-              <p className="text mb-0">{address.state}</p>
+              <p className="text mb10">{address.district}</p>
             </div>
           </div>
         </div>
       ))}
       {/* End col */}
-
-      <div className="col-md-12">
-        <iframe
-          className="position-relative bdrs12 mt30 h250"
-          loading="lazy"
-          src={`https://maps.google.com/maps?q=${addresses[0].address}&t=m&z=14&output=embed&iwloc=near`}
-          title={addresses[0].address}
-          aria-label={addresses[0].address}
-        />
-      </div>
+      {addresses.length > 0 && (
+        <div className="col-md-12">
+          <iframe
+            className="position-relative bdrs12 mt30 h250"
+            loading="lazy"
+            src={`https://maps.google.com/maps?q=${addresses[0].address}, ${addresses[0].district}&t=m&z=14&output=embed&iwloc=near`}
+            title={addresses[0].address}
+            aria-label={addresses[0].address}
+          />
+        </div>
+      )}
       {/* End col */}
     </>
   );
